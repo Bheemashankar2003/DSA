bcc969b8fa540070e7ee7d37cb161463c91470bd
Bheemashankar2003 <bheemashankardiwakar324@gmail.com>
2026-02-19 14:40:22 +0530

    Initial commit

--- Recursion/Natural.java
+++ Recursion/Natural.java
@@ -3,15 +3,25 @@ package Recursion;
 public class Natural {
  
     public static void main(String[] args) {
-        nums(10);
+        // nums(10);
+        num(1);
     }
 
-    public static void nums(int n ){
+    // public static void nums(int n ){
 
-        if(n<=0)
-          return;
+    //     if(n<=0)
+    //       return;
+    //     else
+    //       nums(n-1);
+    //       System.out.println(n);
+    // }
+
+    // OR
+    public static int num(int n){
+        if(n>10)
+          return n;
         else
-          nums(n-1);
           System.out.println(n);
+          return num(n+1);
     }
 }
