b548dc48e13f653ff05832722157a0014bc4d78b
Bheemashankar2003 <bheemashankardiwakar324@gmail.com>
2026-02-23 23:56:47 +0530

    hi

---
+++ Array/Sort/MergeSort.java
@@ -0,0 +1,19 @@
+package Array.Sort;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        
+        int [] arr = {45, 67, 65, 78, 89, 0, 33};
+
+    }
+    public static int[] merge(int arr[]){
+
+        int left = arr.length/2;
+        int right = arr.length-left;
+
+        return arr;
+        
+    }
+
+}
